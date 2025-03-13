6c08b5fed85f8bb03325cfa4ac20137cb3e74ca1
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-13 13:37:48 -0600

    Ask Here Academy Logo was Added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { Button } from "./components/ui/button";
+import AuthPage from "./pages/auth";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <Button className="bg-green-500 p-4 text-white">Tailwind CSS is working! </Button> 
-    </div>
+    <Routes>
+      <Route path="/auth" element={<AuthPage/>}/>
+    </Routes>
   );
 }
 
