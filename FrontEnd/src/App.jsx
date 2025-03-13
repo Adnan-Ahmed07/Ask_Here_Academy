e0ba71e6c6281119c0314a92a7ff4f51e0ef7a82
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-13 13:02:02 -0600

    In Backend Database was Added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="bg-green-500 p-4 text-white">Tailwind CSS is working! </h1>
+      <Button className="bg-green-500 p-4 text-white">Tailwind CSS is working! </Button> 
     </div>
   );
 }
