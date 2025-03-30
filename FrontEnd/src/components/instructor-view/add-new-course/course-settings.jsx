3bc7e8dc0619bb185f698197e5134722439a3477
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-30 01:46:56 -0600

    course settings tested image was uploaded successfully

--- FrontEnd/src/components/instructor-view/add-new-course/course-settings.jsx
+++ FrontEnd/src/components/instructor-view/add-new-course/course-settings.jsx
@@ -1,8 +1,41 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { InstructorContext } from "@/context/instructor-context";
+import { mediaUploadService } from "@/services";
+import { useContext } from "react";
 
 const CourseSettings=()=>{ 
+  const {
+    courseLandingFormData,
+    setCourseLandingFormData,}= useContext(InstructorContext);
+
+    async function handleImageUploadChange(event) {
+      const selectedImage = event.target.files[0];
+  
+      if (selectedImage) {
+        const imageFormData = new FormData();
+        imageFormData.append("file", selectedImage);
+  
+        try {
+          // setMediaUploadProgress(true);
+          const response = await mediaUploadService(
+            imageFormData,
+           
+          );
+          if (response.success) {
+            setCourseLandingFormData({
+              ...courseLandingFormData,
+              image: response.data.url,
+            });
+            // setMediaUploadProgress(false);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+
   return(
     <Card>
     <CardHeader>
@@ -12,17 +45,19 @@ const CourseSettings=()=>{
     
     </div>
     <CardContent>
-     
-        <div className="flex flex-col gap-3">
-          <Label>Upload Course Image</Label>
-          <Input
-          
-            type="file"
-            accept="image/*"
-          />
-        </div>
-      
-    </CardContent>
+        {courseLandingFormData?.image ? (
+          <img src={courseLandingFormData.image} />
+        ) : (
+          <div className="flex flex-col gap-3">
+            <Label>Upload Course Image</Label>
+            <Input
+              onChange={handleImageUploadChange}
+              type="file"
+              accept="image/*"
+            />
+          </div>
+        )}
+      </CardContent>
   </Card>
   );
 }
